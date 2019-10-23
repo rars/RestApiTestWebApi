5b1e3bf709aa6d2a17946b115992d55d45006cfe
Richard Russell <richardr39@gmail.com>
2019-10-23 22:31:19 +0100

    feat: add client library for generating TypeScript

--- RestApiTestWebApi/Startup.fs
+++ RestApiTestWebApi/Startup.fs
@@ -20,7 +20,10 @@ type Startup private () =
     // This method gets called by the runtime. Use this method to add services to the container.
     member this.ConfigureServices(services: IServiceCollection) =
         // Add framework services.
-        services.AddControllers() |> ignore
+        services
+            .AddControllers()
+            .AddNewtonsoftJson() |> ignore
+
         services.AddOpenApiDocument() |> ignore
 
         services.AddScoped<IRestApisRepository, RestApisRepository>() |> ignore
